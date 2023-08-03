664fd387f08cb64679638712ae944cdb88e76da6
Abdurrahman Ashraf <abdoshuruf1@gmail.com>
2023-08-03 14:51:54 +0300

    Commit initial files

---
+++ index.js
@@ -0,0 +1,33 @@
+let home = document.getElementById("home_score").innerText = 0
+
+let guest = document.getElementById("guest_score").innerText = 0
+
+function add1home(){
+ home = home + 1
+ home_score.textContent = home
+}
+
+function add2home(){
+  home = home + 2
+  home_score.textContent = home
+}
+
+function add3home(){
+  home = home + 3
+  home_score.textContent = home
+}
+
+function add1guest(){
+  guest = guest + 1
+  guest_score.textContent = guest
+ }
+ 
+ function add2guest(){
+  guest = guest + 2
+  guest_score.textContent = guest
+ }
+ 
+ function add3guest(){
+  guest = guest + 3
+  guest_score.textContent = guest
+ }
